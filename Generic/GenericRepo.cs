dbcb3d1905d5ce81d1e90471a0a4431c320cf2ad
shamsaly <shamsali88883@gmail.com>
2026-06-13 22:03:47 +0300

    second

---
+++ Generic/GenericRepo.cs
@@ -0,0 +1,44 @@
+﻿using Final_Project.Data;
+using Final_Project.Specification;
+using Microsoft.EntityFrameworkCore;
+
+namespace Final_Project.Generic
+{
+    public class GenericRepo<T> : IgenericRepo<T> where T : class
+    {
+        private readonly HotelDbcontex hotel;
+
+        public GenericRepo(HotelDbcontex hotel)
+        {
+            this.hotel = hotel;
+        }
+
+        public async Task Add(T item)
+        {
+           await hotel.AddAsync(item);
+            await hotel.SaveChangesAsync(); 
+        }
+
+        public async Task Delete(T item)
+        {
+           hotel.Set<T>().Remove(item);
+            await hotel.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<T>> GetAll(ISpec<T> spec)
+        {
+            return await SpecificationEvluator<T>.getquery(hotel.Set<T>(), spec).ToListAsync() ;
+        }
+
+        public async Task<T> GetById(ISpec<T> spec)
+        {
+            return await SpecificationEvluator<T>.getquery(hotel.Set<T>(), spec).FirstOrDefaultAsync();
+        }
+
+        public async Task Update(T item)
+        {
+          hotel.Update(item);
+            await hotel.SaveChangesAsync() ;
+        }
+    }
+}
---
+++ Generic/IgenericRepo.cs
@@ -0,0 +1,19 @@
+﻿using Final_Project.Specification;
+
+namespace Final_Project.Generic
+{
+    public interface IgenericRepo<T>where T : class
+    {
+
+        Task<IEnumerable<T>> GetAll(ISpec<T> spec);
+        Task<T>GetById(ISpec<T> spec);
+
+        Task Add(T item);
+        Task Delete(T item);
+        Task Update(T item);
+
+
+
+
+    }
+}
